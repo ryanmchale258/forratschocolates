f70b02ed50fb9cae1314b00395d005d587b27f2b
ryanmchale258 <ryan.mchale258@gmail.com>
2015-03-16 16:28:51 -0400

    Sass and compass and gulptask

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	concat = require('gulp-concat'),
-	browserify = require('gulp-browserify');
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass');
 
-var jsSources = 'components/scripts/*.js';
+var jsSources = ['components/scripts/*.js'];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('js', function(){
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			images: 'builds/development/images',
+			style: 'expanded',
+			require: ['susy', 'breakpoint']
+		}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest('builds/development/css'));
 });

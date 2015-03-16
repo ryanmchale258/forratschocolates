4d418c51aa83f3db0e2938cd4587c13b88a5abb9
ryanmchale258 <ryan.mchale258@gmail.com>
2015-03-16 16:20:34 -0400

    Added browserify, jQuery, and js gulpfile task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,13 @@
 var gulp = require('gulp'),
-	gulp = require('gulp-util');
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat'),
+	browserify = require('gulp-browserify');
 
+var jsSources = 'components/scripts/*.js';
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(browserify())
+		.pipe(gulp.dest('builds/development/js'));
+});
